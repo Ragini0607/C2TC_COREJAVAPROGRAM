7b5aab6a51572a775b88943a480a173cf25f018a
Ragini Dharmalingam <Ragini Dharmalingam@LAPTOP-7PQEVPA1>
2025-06-15 22:00:01 +0530

    I have performed java program on demonstrate ForLoop

---
+++ C2TC_CoreJavaPrograms/src/day2/ForLoopExample.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class ForLoopExample 
+{
+
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=50 ; i++)    
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+		 
+		for(int j=30; j>=1;j--)
+		{	
+			System.out.print("Value of j = ");
+			System.out.println( j);
+		}
+	}
+
+}
