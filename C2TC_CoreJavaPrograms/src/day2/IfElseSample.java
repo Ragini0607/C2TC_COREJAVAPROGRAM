080753d1b04abd73f0f6aba6f4ecf8a370f0a76e
Ragini Dharmalingam <Ragini Dharmalingam@LAPTOP-7PQEVPA1>
2025-06-15 22:02:08 +0530

    I have performed java program based on conditional statement

---
+++ C2TC_CoreJavaPrograms/src/day2/IfElseSample.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class IfElseSample
+{
+	public static void main(String[] args)
+	{
+		int age=11;
+		System.out.println("if else demonstration");
+		if(age>=18) {
+			System.out.println(" You are eligible for Voting");	}
+		else
+			{System.out.println("Sorry! You need to wait more");}
+
+		
+	}	
+
+}
