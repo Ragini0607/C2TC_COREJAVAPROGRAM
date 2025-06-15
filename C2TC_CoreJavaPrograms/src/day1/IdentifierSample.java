2f47edb16c188f08f8c832d30013152f883a2eac
Ragini Dharmalingam <Ragini Dharmalingam@LAPTOP-7PQEVPA1>
2025-06-15 21:46:43 +0530

    i have performed java program based on identifier

---
+++ C2TC_CoreJavaPrograms/src/day1/IdentifierSample.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class IdentifierSample {
+
+	public static void main(String[] args)
+	{
+		int $number = 20;
+		System.out.println("value of the number variable = "+ $number);
+	
+		String studentName="Ragini"; 
+		System.out.println("value =  "+ studentName);
+	}
+
+}
