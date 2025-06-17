aed8deaccc288d5f5252ff548c1c73390137d495
Ragini Dharmalingam <Ragini Dharmalingam@192.168.137.16>
2025-06-17 09:30:41 +0530

    I have performed java program based on Encapsulation

---
+++ C2TC_CoreJavaPrograms/src/day3/encapsulation/encapsulationDemo.java
@@ -0,0 +1,16 @@
+package day3.encapsulation;
+
+public class encapsulationDemo 
+{
+public static void main(String[] args) {
+		
+		Oopsconcept obj = new Oopsconcept();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+
+}
